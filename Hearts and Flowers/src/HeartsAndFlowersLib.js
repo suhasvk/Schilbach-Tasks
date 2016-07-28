866923632b299686d6d6b50edfd2a0d0e0af2b58
Suhas Vijaykumar <suhasv@Suhass-MacBook-Pro.local>
2016-07-28 12:39:50 -0400

    building Hearts and Flowers model

--- Hearts and Flowers/src/HeartsAndFlowersLib.js
+++ Hearts and Flowers/src/HeartsAndFlowersLib.js
@@ -2,13 +2,18 @@
 // Suhas Vijaykumar, July 2016
 
 // Constants
-CONDITION_HEART_LEFT = 'HL'
-CONDITION_HEART_RIGHT = 'HR'
-CONDITION_FLOWER_LEFT = 'FL'
-CONDITION_FLOWER_RIGHT = 'FR'
+CONDITION_HEART_LEFT = 'HL';
+CONDITION_HEART_RIGHT = 'HR';
+CONDITION_FLOWER_LEFT = 'FL';
+CONDITION_FLOWER_RIGHT = 'FR';
+
+INPUT_LEFT = 'L';
+INPUT_RIGHT = 'R';
 
 // D.O.M. identifiers
-HEART_LEFT_ID = 'heartLeft'
-FLOWER_LEFT_ID = 'flowerLeft'
-HEART_RIGHT_ID = 'heartRight'
-FLOWER_RIGHT_ID = 'flowerRight'
+HEART_LEFT_ID = 'heartLeft';
+FLOWER_LEFT_ID = 'flowerLeft';
+HEART_RIGHT_ID = 'heartRight';
+FLOWER_RIGHT_ID = 'flowerRight';
+
+
--- Hearts and Flowers/src/HeartsAndFlowersModel.js
+++ Hearts and Flowers/src/HeartsAndFlowersModel.js
@@ -1,20 +1,14 @@
 // HeartsAndFlowersGame.js
 // Suhas Vijaykumar, July 2016
 
-// Constants
-CONDITION_HEART_LEFT = 'HL'
-CONDITION_HEART_RIGHT = 'HR'
-CONDITION_FLOWER_LEFT = 'FL'
-CONDITION_FLOWER_RIGHT = 'FR'
-
-randInt = function(min,max){
-	return Math.floor(min + (max-min)*Math.random());
+rand = function(min,max){
+	return min + (max-min)*Math.random();
 }
 
 randomConditionList = function(length){
 	var conditionList = []
 	for (var k = 0; k < length; k++){
-		var conditionNumber = randInt(0,4);
+		var conditionNumber = Math.floor(rand(0,4));
 		var condition;
 		switch (conditionNumber){
 			case 0:
@@ -34,26 +28,112 @@ randomConditionList = function(length){
 	return conditionList;
 }
 
-HeartsAndFlowersTask = function(gameOptions){
-	this.timeInterval = gameOptions.timeInterval;
-	this.numberOfConditions = gameOptions.numberOfConditions;
-	this.displayTimer = gameOptions.displayTimer;
+HeartsAndFlowersTask = function(opts){
+
+	// This is the duration each stimulus is displayed
+	this.appearanceTime = opts.appearanceTime;
+
+	// This is the amount of time which passes after the game starts, before the first stimulus appears
+	this.waitTime = opts.waitTime;
+
+	// The number of stimuli to display
+	this.numberOfConditions = opts.numberOfConditions;
+
+	// Whether or not to display a timer (for debugging / verification purposes)
+	// [not implemented]
+	this.displayTimer = opts.displayTimer;
+
+	this.itervals = []
+	for (var n = 0; n < numberOfConditions; n++){
+		this.intervalList.push(rand(opts.isi.min,opts.isi.max))
+	}
 	this.conditionList = randomConditionList(numberOfConditions);
+
 	this.conditionNumber = -1;
+
+	// These are for logging game info
+	this.results = [];
 	this.gameData = {
 		// Stuff
 	}
+	this.running? = false;
 
-	this.startTask = function(data){
-		// Stuff
+	this.getStartTime = function(){
+		return this.startTime;
+	}
+
+	this.isCompatible = function(inpt, condition){
+		return (((inpt == 'R') && (condition == 'HR' || condition == 'FL')) 
+			|| ((inpt == 'L') && (condition == 'HL' || condition == 'FR')));
+	}
+
+
+	this.logMove = function(data){
+		this.results.push({
+			"type": "INPUT",
+			"time": data.time,
+			"direction": data.input,
+			"condition": data.condition,
+			"correct?": this.isCompatible(data.input, data.condition);
+		});
+	}
+
+	this.logDisplay = function(data){
+		this.results.push({
+			"type": "OUTPUT",
+			"time": data.time,
+			"condition": data.condition
+		});
+	}
+
+	this.startTask = function(){
+		this.conditionNumber = 0;
+		this.running = true;
+		this.dispatchEvent('start',{
+			"number": this.conditionNumber,
+			"newCondition": this.conditionList[this.conditionNumber],
+		})
+
 	}
 
 	this.makeMove = function(data){
-		// Stuff
+		this.conditionNumber++;
+		this.dispatchEvent('move',{
+			"number": this.conditionNumber,
+			"newCondition": this.conditionList[this.conditionNumber],
+		});
 	}
 
 	this.getCondition = function(){
 		return conditionList[conditionNumber];
 	}
-		
+
+	////////////////////////////
+	// EVENT LISTENING INTERFACE
+	// 
+
+	this.allHandlers = new Array();
+
+	/*
+	 * Dispatch a new event to all the event listeners of a given event type
+	 */
+	this.dispatchEvent = function(type, details){
+		var newEvent = new BoardEvent(type, details);
+
+		if (this.allHandlers[type]){
+			for (var i in this.allHandlers[type]){
+				this.allHandlers[type][i](newEvent);
+			}
+		}
+	}
+
+	/**
+	 * Add a new event listener for a given event type
+	 * the parameter 'handler' has to be a function with one parameter which is an event object
+	 */
+	this.addEventListener = function(eventType, handler){
+		if (!this.allHandlers[eventType])
+			this.allHandlers[eventType] = [];
+		this.allHandlers[eventType].push(handler);
+	}		
 }
